dc07c39c5347c3977410fce2e3ba2a521b4d9287
Nika Nabakhteveli <70432974+nNabakhteveli@users.noreply.github.com>
2021-02-16 17:09:42 +0400

    Adding number validation on inputs

--- src/com/bitcamper/Main.java
+++ src/com/bitcamper/Main.java
@@ -9,18 +9,29 @@ public class Main {
         final byte PERCENT = 100;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal;
+        while(true){
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if(principal >= 1_000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1000 and 1000000.");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
+        float annualInterest;
+        while(true){
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            if(annualInterest > 0 && annualInterest <= 30)
+                break;
+            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+        }
 
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
         System.out.print("Period (Years): ");
-
         byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
 
+        int numberOfPayments = years * MONTHS_IN_YEAR;
         double mortgage = principal
                 *(monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
